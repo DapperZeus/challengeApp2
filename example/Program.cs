79d55bd3b68c0b8ceadec7d552ec6d0bcfe27c97
Matthew Parrott <matthewp25@learn.cssd.ab.ca>
2019-02-26 14:16:49 -0700

    first commit

---
+++ example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 9;
+
+            string alsoNum = "0";
+
+            var result = myNum + Convert.ToInt16(alsoNum);
+
+            Console.WriteLine(result);
+        }
+    }
+}
